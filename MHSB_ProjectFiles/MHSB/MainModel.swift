6b68484d2d9fae2ee64fe7612febc6211c6343f2
stevesl <stevesl@umich.edu>
2017-10-08 08:00:42 -0400

    New Files

---
+++ MHSB_ProjectFiles/MHSB/MainModel.swift
@@ -0,0 +1,97 @@
+//
+//  MainModel.swift
+//  MHSB
+//
+//  Created by Steve Lee on 9/1/17.
+//  Copyright © 2017 Steve Lee. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+struct MainModel {
+
+    var studentsData = [Student]()
+    var isLoaded = Bool()
+    
+    mutating func loadData() {
+        //Loading data and seeing if a dataset has already been added. If not,
+        //return false.
+        let defaults = UserDefaults.standard
+        let storedData = defaults.object(forKey: "MHS_Data") as? [Student]
+        if storedData != nil {
+            isLoaded = true
+        } else {
+            isLoaded = false
+        }
+        addNewData()
+    }
+    
+    //Adding new spreadsheet data from email.
+    //Change the parameter later when implementing email import feature.
+    mutating func addNewData() {
+        //Dummy values.
+        //TEMPPPPPPPPPPPPPP
+        let path = Bundle.main.path(forResource: "MHS_0", ofType: "csv")
+        let rawCSVData = try! String.init(contentsOf: URL.init(fileURLWithPath: path!))
+
+        parseCSVFile(rawData: rawCSVData)
+        
+        /*//Converting CSV file to String
+        let csvURL = NSURL(fileURLWithPath: Bundle.main.path(forResource: "1", ofType: "csv")!)
+        
+        do {
+            rawCSVData = try String(contentsOf: csvURL as URL);
+            //print("Imported Data" + "\(rawCSVData)");
+        } catch {
+            print("Corrupted File");
+        }*/
+          
+        
+    }
+    
+    mutating func parseCSVFile(rawData: String) {
+        /*Parsing*/
+        
+        //Make this more flexible later on by allowing the user to put
+        //the categories in any order they want in the csv file.
+        
+        print(rawData)
+        let separatedByStudents:[String] = rawData.components(separatedBy: "\r");
+        
+        for studentData in separatedByStudents {
+            
+            var student: Student = Student()
+            let data:[String] = studentData.components(separatedBy: "\t")//Look up what \t is and if it's okay to use instead of ,
+            
+            if data[0] != "Parade Position" {
+                student.rank = Int(Double(data[0])!.truncatingRemainder(dividingBy: 10.0))
+                student.location = Int(Double(data[0])!.truncatingRemainder(dividingBy: 1.0) * 10)//
+                student.lastName = data[1]
+                student.firstName = data[2]
+                student.instrument = data[3]
+                student.period = Int(Double(data[4])!.truncatingRemainder(dividingBy: 10.0))
+                student.grade = Int(Double(data[4])!.truncatingRemainder(dividingBy: 1.0) * 10)//Can't get the decimal value.
+                
+                studentsData.append(student)
+            }
+        }
+        print(studentsData[0].location!)
+    }
+    
+    struct Student {
+        var firstName: String? = ""
+        var lastName: String? = ""
+        var grade: Int? = 0
+        var instrument: String? = ""
+        var period: Int? = 0
+        var rank: Int? = 0
+        var location: Int? = 0
+    }
+    
+    /*//This happens when the button is pressed.
+    mutating func addData() {
+        
+    }*/
+    
+}
---
+++ MHSB_ProjectFiles/MHSB/MainViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  MHSB
+//
+//  Created by Steve Lee on 8/31/17.
+//  Copyright © 2017 Steve Lee. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+            updateTextGUI()
+    }
+    
+    func updateTextGUI() {
+        //Load data.
+        var dataModel = MainModel()
+        dataModel.loadData()
+        
+        //Loading saved data. Check if any data is saved. If yes, load it as GUI.
+        //This probably will not pass only when no data is present in the beginning.
+        if dataModel.isLoaded {
+            //Use functions from MainModel to get the loaded data and present them
+            //on the device.
+            
+            
+        //No data is present in the system.
+        } else {
+            //Show in the GUI that no data is present and show the button
+            //to add data/teach the user how to add data.
+            
+            print("No Data")
+            
+        }
+     
+        
+    }
+    
+    //Function that reloads the text data everytime the scrolllist selector
+    //is changed.
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
